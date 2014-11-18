cc125a91b915f6db87e0ede616071ea762bcbbf6
corgom <corgom+github@gmail.com>
2014-11-18 11:52:46 -0600

    Create php_if_1.php

---
+++ php_if_1.php
@@ -0,0 +1,20 @@
+<?php
+// uso de IF para controlar salidas bloques HTML
+?>
+
+<?php
+$imprimirSaludo = TRUE;
+?>
+
+<?php
+
+if ($imprimirSaludo)
+{
+?>
+  <p>Saludos!</p>
+<?php
+}
+
+?>
+
+<p>Texto normal html</p>
